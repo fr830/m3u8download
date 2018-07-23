626e57ac05cc56352ac00f415a116f6f8a9b11a8
yuan hai zhao <zhaoyuanhai66@outlook.com>
2018-07-23 09:31:06 +0800

    init

---
+++ m3u8download/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace m3u8download
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_url = new System.Windows.Forms.TextBox();
+            this.lv_list = new System.Windows.Forms.ListView();
+            this.col_path = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.col_pasent = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_msg = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_number = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_download = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_download_path = new System.Windows.Forms.TextBox();
+            this.btn_dialog = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.progbar_1 = new System.Windows.Forms.ProgressBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "地址";
+            // 
+            // txt_url
+            // 
+            this.txt_url.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_url.Location = new System.Drawing.Point(98, 13);
+            this.txt_url.Name = "txt_url";
+            this.txt_url.Size = new System.Drawing.Size(560, 25);
+            this.txt_url.TabIndex = 1;
+            // 
+            // lv_list
+            // 
+            this.lv_list.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lv_list.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.col_path,
+            this.col_pasent});
+            this.lv_list.GridLines = true;
+            this.lv_list.Location = new System.Drawing.Point(15, 376);
+            this.lv_list.Name = "lv_list";
+            this.lv_list.Size = new System.Drawing.Size(641, 279);
+            this.lv_list.TabIndex = 2;
+            this.lv_list.UseCompatibleStateImageBehavior = false;
+            this.lv_list.View = System.Windows.Forms.View.Details;
+            // 
+            // col_path
+            // 
+            this.col_path.Text = "路径";
+            this.col_path.Width = 418;
+            // 
+            // col_pasent
+            // 
+            this.col_pasent.Text = "下载进度";
+            this.col_pasent.Width = 115;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "文件信息";
+            // 
+            // txt_msg
+            // 
+            this.txt_msg.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_msg.Location = new System.Drawing.Point(15, 166);
+            this.txt_msg.Multiline = true;
+            this.txt_msg.Name = "txt_msg";
+            this.txt_msg.ReadOnly = true;
+            this.txt_msg.Size = new System.Drawing.Size(641, 175);
+            this.txt_msg.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 357);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "下载信息";
+            // 
+            // txt_number
+            // 
+            this.txt_number.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_number.Location = new System.Drawing.Point(97, 44);
+            this.txt_number.Name = "txt_number";
+            this.txt_number.Size = new System.Drawing.Size(479, 25);
+            this.txt_number.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "下载线程数";
+            // 
+            // btn_download
+            // 
+            this.btn_download.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_download.Location = new System.Drawing.Point(581, 44);
+            this.btn_download.Name = "btn_download";
+            this.btn_download.Size = new System.Drawing.Size(75, 26);
+            this.btn_download.TabIndex = 8;
+            this.btn_download.Text = "下载";
+            this.btn_download.UseVisualStyleBackColor = true;
+            this.btn_download.Click += new System.EventHandler(this.btn_download_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "文件路径";
+            // 
+            // txt_download_path
+            // 
+            this.txt_download_path.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_download_path.Location = new System.Drawing.Point(97, 77);
+            this.txt_download_path.Name = "txt_download_path";
+            this.txt_download_path.ReadOnly = true;
+            this.txt_download_path.Size = new System.Drawing.Size(479, 25);
+            this.txt_download_path.TabIndex = 10;
+            // 
+            // btn_dialog
+            // 
+            this.btn_dialog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_dialog.Location = new System.Drawing.Point(581, 76);
+            this.btn_dialog.Name = "btn_dialog";
+            this.btn_dialog.Size = new System.Drawing.Size(75, 26);
+            this.btn_dialog.TabIndex = 11;
+            this.btn_dialog.Text = "选择";
+            this.btn_dialog.UseVisualStyleBackColor = true;
+            this.btn_dialog.Click += new System.EventHandler(this.btn_dialog_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // progbar_1
+            // 
+            this.progbar_1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progbar_1.Location = new System.Drawing.Point(97, 108);
+            this.progbar_1.Name = "progbar_1";
+            this.progbar_1.Size = new System.Drawing.Size(559, 23);
+            this.progbar_1.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "下载进度";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 668);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.progbar_1);
+            this.Controls.Add(this.btn_dialog);
+            this.Controls.Add(this.txt_download_path);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_download);
+            this.Controls.Add(this.txt_number);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_msg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lv_list);
+            this.Controls.Add(this.txt_url);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "M3u8DownLoad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_url;
+        private System.Windows.Forms.ListView lv_list;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_msg;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_number;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_download;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_download_path;
+        private System.Windows.Forms.Button btn_dialog;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ColumnHeader col_path;
+        private System.Windows.Forms.ColumnHeader col_pasent;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ProgressBar progbar_1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ m3u8download/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Threading;
+using System.Net;
+using System.IO;
+
+namespace m3u8download
+{
+    public partial class Form1 : Form
+    {
+        HttpClient client;
+        List<Thread> threads;
+        List<string> files;
+        List<string> list;
+        Dictionary<string, int> parsent;
+        string baseUrl;
+        int index = 0;
+        int count;
+
+        private int SpeedOfProgress
+        {
+            get
+            {
+                return (int)(Math.Round(this.parsent.Where(x => x.Value == 100).Count() / (decimal)this.files.Count, 2) * 100);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.client = new HttpClient();
+            this.threads = new List<Thread>();
+            this.files = new List<string>();
+            this.parsent = new Dictionary<string, int>();
+            this.list = new List<string>();
+            timer1.Tick += Timer1_Tick;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (count <= int.Parse(this.txt_number.Text) && index < this.files.Count)
+            {
+                DownLoadFile(this.baseUrl + "/" + this.files[index++]);
+            }
+
+            var th = new Thread(new ThreadStart(() =>
+            {
+                this.Invoke(new Action(() =>
+                {
+                    foreach (var temp in this.parsent)
+                    {
+
+                        if (this.list.Contains(temp.Key))
+                            goto Label;
+
+                        var item = this.lv_list.FindItemWithText(temp.Key);
+                        if (item != null)
+                        {
+                            item.SubItems[1].Text = temp.Value == -1 ? "下载出错" : temp.Value + "%";
+                            if (temp.Value == 100)
+                                list.Add(temp.Key);
+                        }
+
+                        Label:;
+                    }
+
+                    this.progbar_1.Value = this.SpeedOfProgress;
+                }));
+            }));
+            th.Start();
+        }
+
+        private async void btn_download_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(this.txt_number.Text, out int a))
+            {
+                MessageBox.Show("线程数必须为");
+                return;
+            }
+
+            string strData = null;
+            try
+            {
+                strData = await this.client.GetStringAsync(this.txt_url.Text);
+            }
+            catch
+            {
+                this.txt_msg.Text += "下载文件失败，正在重试\r\n";
+            }
+            if (string.IsNullOrWhiteSpace(strData))
+            {
+                this.btn_download.Enabled = true;
+            }
+            else
+            {
+                this.baseUrl = this.txt_url.Text.Substring(0, this.txt_url.Text.LastIndexOf("/"));
+                ShowInfo(strData);
+                this.files = GetFilePath(strData);
+                this.InitListView();
+                timer1.Start();
+            }
+        }
+
+        private void InitListView()
+        {
+            foreach (var path in this.files)
+            {
+                var item = new ListViewItem();
+                item.Text = path;
+                item.SubItems.Add("0%");
+                this.lv_list.Items.Add(item);
+            }
+        }
+
+        private void ShowInfo(string strData)
+        {
+            this.txt_msg.Text += "已找到m3u8文件\r\n";
+            var files = GetFilePath(strData);
+            this.txt_msg.Text += $"共有{files.Count}个文件\r\n";
+            this.txt_msg.Text += "准备下载.....\r\n";
+            this.txt_msg.Text += "已下载0%";
+        }
+
+        private List<string> GetFilePath(string strData)
+        {
+            return strData.Split('\n').Where(x => x.EndsWith(".ts")).ToList();
+        }
+
+        private async void DownLoadFile(string filepath)
+        {
+            lock (this)
+            {
+                count++;
+            }
+            string fileName = Path.GetFileName(filepath);
+            this.parsent.Add(fileName, 0);
+            var responseMessage = await client.GetAsync(filepath);
+            var bys = new List<byte>();
+            try
+            {
+                using (var stream = await responseMessage.Content.ReadAsStreamAsync())
+                {
+                    var length = stream.Length;
+                    while (bys.Count <= length)
+                    {
+                        bys.Add((byte)stream.ReadByte());
+                        this.parsent[fileName] = (int)(Math.Round((bys.Count / (decimal)length), 2) * 100);
+                    }
+                }
+            }
+            catch
+            {
+                this.parsent[fileName] = -1;
+            }
+            lock (this)
+            {
+                count--;
+            }
+            using (var fileStream = new FileStream($"{this.txt_download_path.Text}\\{fileName}", FileMode.Create, FileAccess.Write))
+            {
+                fileStream.Write(bys.ToArray(), 0, bys.Count);
+            }
+        }
+
+        private void btn_dialog_Click(object sender, EventArgs e)
+        {
+            var result = this.folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                this.txt_download_path.Text = this.folderBrowserDialog1.SelectedPath;
+            }
+        }
+    }
+}
